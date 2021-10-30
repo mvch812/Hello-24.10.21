0a89c1586d191da7e96fb0b8f4342140e65177f9
MVCH812 <mvch812@gmail.com>
2021-10-30 09:32:39 +0300

    Написали программу про собаку

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,4 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
-Проверка
+// Задача: два друга идут навстречу друг другу. От одного к другому бегает собака. Вопрос: сколько раз собака сбегает от одного
+//к другому пока они не встретятся?
+
+int count = 0;
+int distance = 10000;
+int FirstFriendSpeed =1;
+int SecondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+
+        int time = distance/( FirstFriendSpeed + dogSpeed );
+        friend = 2;
+    }
+    else
+    {
+        int time = distance/( SecondFriendSpeed + dogSpeed );
+        friend = 1;
+    }
+distance = distance - (FirstFriendSpeed + SecondFriendSpeed)* time;
+count++
+
+}
+
+writeline (count);
